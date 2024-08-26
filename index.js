2299139f149882ec870c348a5e38a64c32d333db
abu bakar <abubakar850772@gmail.com>
2024-08-26 17:59:48 +0600

    implement post method for added a food item

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
-const app = express()
+const app = express();
 
-// middlewares 
+// middlewares
 
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 console.log(process.env.DB_USER);
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a2ulpwj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -23,7 +20,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -31,104 +28,112 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    
-    const foodsCollection = client.db("restaurant_management").collection("foods")
-    const purchasesCollection = client.db("restaurant_management").collection("purchases")
-    const galleryCollection = client.db("restaurant_management").collection("gallery")
+    const foodsCollection = client
+      .db("restaurant_management")
+      .collection("foods");
+    const purchasesCollection = client
+      .db("restaurant_management")
+      .collection("purchases");
+    const galleryCollection = client
+      .db("restaurant_management")
+      .collection("gallery");
 
-    // get all foods 
+      //add food in food collection
 
-    app.get("/foods", async(req, res) => {
-        const cursor = foodsCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
-    })
+      app.post("/foods", async (req, res) => {
+        const docs = req.body;
+        console.log(docs);
+  
+        const result = await foodsCollection.insertOne(docs);
+        res.send(result);
+      });
 
-        // get foods by foodName
+    // get all foods
 
-        app.get("/foods/:name", async(req, res) => {
-          const name = req.params.name;
-          // console.log(name);
-          const query = {foodName:  { $regex: new RegExp(name, "i") }};
-          const result = await foodsCollection.find(query).toArray()
-          
-          res.send(result);
-          
-        })
+    app.get("/foods", async (req, res) => {
+      const cursor = foodsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-        // get foods by id
+    // get foods by foodName
 
-        app.get("/food/:id", async(req, res) => {
-          const id = req.params.id;
-          // console.log(id);
-          const query = {_id: new ObjectId(id)};
-          const result =  await foodsCollection.findOne(query) 
-          res.send(result);
-          
-        })
-        // get foods by email
+    app.get("/foods/:name", async (req, res) => {
+      const name = req.params.name;
+      // console.log(name);
+      const query = { foodName: { $regex: new RegExp(name, "i") } };
+      const result = await foodsCollection.find(query).toArray();
 
-        app.get("/foods/user/:email", async(req, res) => {
-          const emailId = req.params.email;
-          console.log(emailId);
-          const query = {email: emailId};
-          const result =  await foodsCollection.find(query).toArray()
-          res.send(result);
-          
-        })
+      res.send(result);
+    });
 
-        // update foods item 
+    // get foods by id
 
-        app.patch("/update/:id", async(req, res) => {
-          const data = req.body;
-          const id = req.params.id;
-          console.log("update info",data);
-          console.log("food id",id);
-          const filter = { _id: new ObjectId(id) };
-          const updateDocs = {
-            $set: data
-          }
-          const result = await foodsCollection.updateOne(filter, updateDocs)
-          res.send(result)
-          
-        })
+    app.get("/food/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await foodsCollection.findOne(query);
+      res.send(result);
+    });
+    // get foods by email
 
-        // insert purchase details 
+    app.get("/foods/user/:email", async (req, res) => {
+      const emailId = req.params.email;
+      console.log(emailId);
+      const query = { email: emailId };
+      const result = await foodsCollection.find(query).toArray();
+      res.send(result);
+    });
 
-        app.post("/purchase", async (req, res) => {
-          const docs = req.body;
-          console.log(docs);
-          
-          const result = await purchasesCollection.insertOne(docs);
-          res.send(result);
-        })
+    // update foods item
 
-        //insert gallery info
+    app.patch("/update/:id", async (req, res) => {
+      const data = req.body;
+      const id = req.params.id;
+      console.log("update info", data);
+      console.log("food id", id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDocs = {
+        $set: data,
+      };
+      const result = await foodsCollection.updateOne(filter, updateDocs);
+      res.send(result);
+    });
 
-        app.post("/gallery", async (req, res) => {
-          const docs = req.body;
-          console.log(docs);
-          
-          const result = await galleryCollection.insertOne(docs);
-          res.send(result);
-        })
+    // insert purchase details
 
-        // get data from gallery collection 
+    app.post("/purchase", async (req, res) => {
+      const docs = req.body;
+      console.log(docs);
 
-        app.get("/gallery", async(req, res) => {
-          const cursor = galleryCollection.find()
-          const result = await cursor.toArray()
-          res.send(result)
-      })
+      const result = await purchasesCollection.insertOne(docs);
+      res.send(result);
+    });
 
+    //insert gallery info
 
+    app.post("/gallery", async (req, res) => {
+      const docs = req.body;
+      console.log(docs);
 
+      const result = await galleryCollection.insertOne(docs);
+      res.send(result);
+    });
 
+    // get data from gallery collection
 
+    app.get("/gallery", async (req, res) => {
+      const cursor = galleryCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -136,17 +141,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-
-
 app.get("/", (req, res) => {
-    res.send("server is running ")
-})
+  res.send("server is running ");
+});
 
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
-    
-})
+  console.log(`server is running on port ${port}`);
+});
